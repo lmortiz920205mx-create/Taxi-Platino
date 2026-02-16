12092bee36982de684e94af64d1ccf6b6ce49a34
lmortiz920205mx-create <lmortiz920205mx@gmail.com>
2026-02-16 02:26:06 -0600

    Delete data.js

--- data.js
+++
@@ -1,5 +0,0 @@
-const data = [
-{ title: "Servicios Locales", description: "Consulta tarifas por sector y colonia." },
-{ title: "Viajes Foráneos", description: "Precios actualizados con casetas incluidas." },
-{ title: "Servicio Seguro", description: "Conductores certificados y unidades monitoreadas." }
-];
