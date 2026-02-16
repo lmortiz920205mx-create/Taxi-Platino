fb6f5fe39805da34850e847b90227d92d9317e22
lmortiz920205mx-create <lmortiz920205mx@gmail.com>
2026-02-15 23:37:15 -0600

    Add files via upload

--- sw.js
+++
@@ -1,8 +0,0 @@
-
-const CACHE="taxiplatino-prod-v1";
-self.addEventListener("install",e=>{
-e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["/","/index.html","/styles.css","/app.js"])))
-});
-self.addEventListener("fetch",e=>{
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
-});
