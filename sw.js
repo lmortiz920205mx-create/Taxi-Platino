d0df0ebd415390ccda60af7a3975428644e6fbc9
lmortiz920205mx-create <lmortiz920205mx@gmail.com>
2026-02-17 21:49:52 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia el número de versión cada vez que hagas cambios en el HTML
-const CACHE_NAME = 'taxi-platino-v2'; 
+const CACHE_NAME = 'taxi-platino-v3'; 
 
 const urlsToCache = [
   './',
